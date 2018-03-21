10894cfe089914beefa3ce6fc0a7aad3c8b79d12
Maikel Linke <mkllnk@web.de>
2018-03-21 16:32:15 +1100

    Updated method signatures according to interface
    These were generating warnings in the log file on PHP7. But honestly,
    two of those functions don't seem to do anything.

--- lib/walker.php
+++ lib/walker.php
@@ -3,11 +3,33 @@
 // https://sridharkatakam.com/display-custom-menu-select-dropdown-wordpress/
 class Walker_Nav_Menu_Select extends Walker_Nav_Menu {
 
-    function start_lvl(&$output, $depth) {
+    /**
+     * Starts the list before the elements are added.
+     *
+     * @since 3.0.0
+     *
+     * @see Walker::start_lvl()
+     *
+     * @param string   $output Passed by reference. Used to append additional content.
+     * @param int      $depth  Depth of menu item. Used for padding.
+     * @param stdClass $args   An object of wp_nav_menu() arguments.
+     */
+    public function start_lvl(&$output, $depth = 0, $args = array()) {
         $indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
     }
 
-    function end_lvl(&$output, $depth) {
+    /**
+     * Ends the list of after the elements are added.
+     *
+     * @since 3.0.0
+     *
+     * @see Walker::end_lvl()
+     *
+     * @param string   $output Passed by reference. Used to append additional content.
+     * @param int      $depth  Depth of menu item. Used for padding.
+     * @param stdClass $args   An object of wp_nav_menu() arguments.
+     */
+    public function end_lvl(&$output, $depth = 0, $args = array()) {
         $indent = str_repeat("\t", $depth); // don't output children closing tag
     }
 
@@ -18,14 +40,27 @@ class Walker_Nav_Menu_Select extends Walker_Nav_Menu {
      * @param  object $item   Menu item data object.
      * @param  int $depth     Depth of menu item. May be used for padding.
      * @param  array $args    Additional strings.
+     * @param int      $id     Current item ID.
      * @return void
      */
-    function start_el(&$output, $item, $depth, $args) {
+    public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
         $url = '#' !== $item->url ? $item->url : '';
         $output .= '<option value="' . $url . '">' . $item->title;
     }
 
-    function end_el(&$output, $item, $depth) {
+    /**
+     * Ends the element output, if needed.
+     *
+     * @since 3.0.0
+     *
+     * @see Walker::end_el()
+     *
+     * @param string   $output Passed by reference. Used to append additional content.
+     * @param WP_Post  $item   Page data object. Not used.
+     * @param int      $depth  Depth of page. Not Used.
+     * @param stdClass $args   An object of wp_nav_menu() arguments.
+     */
+    public function end_el(&$output, $item, $depth = 0, $args = array()) {
         $output .= "</option>\n"; // replace closing </li> with the option tag
     }
 
