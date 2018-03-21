0d04dfa025c70d60f6ca203e9ea8699b26260ec8
Maikel Linke <mkllnk@web.de>
2018-03-21 16:29:35 +1100

    Indent by convention

--- lib/walker.php
+++ lib/walker.php
@@ -2,29 +2,31 @@
 
 // https://sridharkatakam.com/display-custom-menu-select-dropdown-wordpress/
 class Walker_Nav_Menu_Select extends Walker_Nav_Menu {
-  function start_lvl(&$output, $depth){
-    $indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
-  }
 
-  function end_lvl(&$output, $depth){
-    $indent = str_repeat("\t", $depth); // don't output children closing tag
-  }
+    function start_lvl(&$output, $depth) {
+        $indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
+    }
 
-  /**
-   * Start the element output.
-   *
-   * @param  string $output Passed by reference. Used to append additional content.
-   * @param  object $item   Menu item data object.
-   * @param  int $depth     Depth of menu item. May be used for padding.
-   * @param  array $args    Additional strings.
-   * @return void
-   */
-  function start_el(&$output, $item, $depth, $args) {
-    $url = '#' !== $item->url ? $item->url : '';
-    $output .= '<option value="' . $url . '">' . $item->title;
-  }	      
+    function end_lvl(&$output, $depth) {
+        $indent = str_repeat("\t", $depth); // don't output children closing tag
+    }
 
-  function end_el(&$output, $item, $depth){
-    $output .= "</option>\n"; // replace closing </li> with the option tag
-  }
+    /**
+     * Start the element output.
+     *
+     * @param  string $output Passed by reference. Used to append additional content.
+     * @param  object $item   Menu item data object.
+     * @param  int $depth     Depth of menu item. May be used for padding.
+     * @param  array $args    Additional strings.
+     * @return void
+     */
+    function start_el(&$output, $item, $depth, $args) {
+        $url = '#' !== $item->url ? $item->url : '';
+        $output .= '<option value="' . $url . '">' . $item->title;
+    }
+
+    function end_el(&$output, $item, $depth) {
+        $output .= "</option>\n"; // replace closing </li> with the option tag
+    }
+
 }
